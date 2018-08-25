ba3f8774c86dba0043e4ce8c6fe9353342ac5304
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2018-08-25 21:45:31 +0200

    ability to sett multiple bits

--- include/ucpp/bit.h
+++ include/ucpp/bit.h
@@ -6,9 +6,20 @@ namespace ucpp
 namespace bit
 {
 
+namespace detail {
 template<unsigned int B, typename T>
-void set(T &value) {
-    value = value | (1 << B);
+constexpr T set(T value) {
+    return value | (1 << B);
+}
+}
+
+template<typename T>
+constexpr void set(T &) {}
+
+template<unsigned int B, unsigned int... Bs, typename T>
+constexpr void set(T &value) {
+    value = detail::set<B>(value);
+    set<Bs...>(value);
 }
 
 }
--- tests/tests_bit.cxx
+++ tests/tests_bit.cxx
@@ -7,3 +7,10 @@ TEST_CASE(bit_set)
     ucpp::bit::set<3>(foo);
     CHECK(foo == 0xA8);
 }
+
+TEST_CASE(bit_set_multiple)
+{
+    unsigned int foo = 0xA0;
+    ucpp::bit::set<0, 1>(foo);
+    CHECK(foo == 0xA3);
+}
